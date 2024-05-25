b0cc2e547fd9cb2c584120c50a854939c6442c6a
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-05-25 18:04:10 +0530

    add all CRUD todo controllers

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -6,12 +6,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const addTodo = asyncHandler(async (req, res) => {
     const { title, description, due } = req.body;
 
-    console.log(title);
-
-    console.log(description);
-
-    console.log(due);
-
     const addedTodo = await Todo.create({
         title,
         description,
@@ -28,4 +22,79 @@ const addTodo = asyncHandler(async (req, res) => {
     )
 })
 
-export { addTodo };
+const getTodos = asyncHandler(async (req, res) => {
+    
+    const allTodos = await Todo.find({});
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                allTodos,
+                "All todos fetched success fully"
+        )
+    )
+})
+
+const getSpecificTodo = asyncHandler(async (req, res) => {
+    const id = req.params?.id;
+
+    const todo = await Todo.findById({
+        _id:id
+    })
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                todo,
+                "Successfully fetched todo"
+        )
+    )
+})
+
+const updateTodo = asyncHandler(async (req, res) => {
+    
+
+    const updatedTodo = await Todo.findByIdAndUpdate(
+        req.params?.id,
+        {
+            $set: {
+                title: req.body?.title,
+                description: req.body?.description,
+                due:req.body?.due
+            }
+        },
+        {
+            new:true
+        }
+    ).select("-createdAt")
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updatedTodo,
+                "Successfully updated Todo"
+        )
+    )
+})
+
+const deleteTodo = asyncHandler(async (req, res) => {
+    const id = req.params?.id
+
+    const todo = await Todo.findByIdAndDelete({
+        _id:id
+    })
+
+    return res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Successfully deleted todo"
+        )
+    )
+})
+export { addTodo, deleteTodo, getSpecificTodo, getTodos, updateTodo };
+
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { addTodo } from "../controllers/todo.controller.js";
+import { addTodo, deleteTodo, getSpecificTodo, getTodos, updateTodo, } from "../controllers/todo.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/tasks").post(upload.none(),addTodo);
+router.route("/tasks").post(upload.none(), addTodo);
+router.route("/tasks").get(getTodos)
+router.route("/tasks/:id").get(getSpecificTodo)
+router.route("/tasks/:id").put(upload.none(),updateTodo)
+router.route("/tasks/:id").delete(deleteTodo)
 
 export default router
